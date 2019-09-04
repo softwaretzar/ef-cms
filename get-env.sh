c4fbbd18f369c07f45590bdad43e82bc638e0465
Joseph Sutton <jsutton@flexion.us>
2019-09-04 09:12:57 -0500

    env fallback

--- get-env.sh
+++ get-env.sh
@@ -8,8 +8,6 @@ elif [[ $BRANCH == 'staging' ]] ; then
   echo 'stg'
 elif [[ $BRANCH == 'master' ]] ; then
   echo 'prod'
-elif [[ $BRANCH == 'circleci-deployment-refactor' ]] ; then
-  echo 'joe'
 else
-  exit 1;
+  echo 'joe';
 fi
