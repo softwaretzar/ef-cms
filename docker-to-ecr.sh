980175678b69a396912ad2a37870eb308b947958
Joseph Sutton <jsutton@flexion.us>
2019-12-17 10:16:50 -0600

    modified ecr deployment script to fetch aws account id if it doesnt exist

--- docker-to-ecr.sh
+++ docker-to-ecr.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-ENVIRONMENT=$1
+
+# get aws account id if it does not exist in an env var
+AWS_ACCOUNT_ID=${AWS_ACCOUNT_ID:-`aws sts get-caller-identity --query "Account"`}
+AWS_ACCOUNT_ID="${AWS_ACCOUNT_ID%\"}"
+AWS_ACCOUNT_ID="${AWS_ACCOUNT_ID#\"}"
 
 $(aws ecr get-login --no-include-email --region us-east-1)
-docker build -t "ef-cms-${ENVIRONMENT}-us-east-1" -f Dockerfile-CI .
-docker tag "ef-cms-${ENVIRONMENT}-us-east-1:latest" "515554424717.dkr.ecr.us-east-1.amazonaws.com/ef-cms-${ENVIRONMENT}-us-east-1:latest"
-docker push "515554424717.dkr.ecr.us-east-1.amazonaws.com/ef-cms-${ENVIRONMENT}-us-east-1:latest"
+docker build -t "ef-cms-us-east-1" -f Dockerfile-CI .
+docker tag "ef-cms-us-east-1:latest" "$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/ef-cms-us-east-1:latest"
+docker push "$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/ef-cms-us-east-1:latest"
 
